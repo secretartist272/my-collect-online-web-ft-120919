b8eb548fd9c14d204817e103472be072d0673fc4
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-17 21:56:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(empty_array)
   i = 0 
   name_collection = []
   while i < empty_array.length
-    name_collection.push yield(array[i])
+    name_collection << yield(array[i])
     i += 1 
   end
   name_collection
