f7a57111517795eac426126134df7c3b7c1c0eed
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-17 22:01:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,8 @@
 counter = 0
 def my_collect(empty_array)
   counter = 0 
-  new_collection = []
-  while counter < empty_array.length
-    new_collection << yield(array[counter])
     counter =+ 1 
-  end
-  new_collection
+ 
 end
 
 languages = ["ruby", "javascript", "python", "objective-c"]
